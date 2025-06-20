530003ac72bd12bba77d9009bb7db1234959c4a3
Gaudv <gitlab.g@canque.fr>
2025-06-20 11:25:17 +0200

    reformat

--- index.php
+++ index.php
@@ -12,48 +12,52 @@ $flightlist = json_decode(file_get_contents($airtrail_baseurl . "/api/flight/lis
 
 <!doctype html>
 <html lang="en-US">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
     <title>My flights</title>
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-	<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-	<script src="https://cdn.jsdelivr.net/npm/leaflet.geodesic"></script>
-	  
-	  
-	<style>
-		html, body {
-			height: 100%;
-			margin: 0;
-		}
-		.leaflet-container {
-			height: 100%;
-			width: 100%;
-			max-width: 100%;
-			max-height: 100%;
-		}
-	</style>
-  </head>
-  <body>
-     <div id="map"></div>
-		  <script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/leaflet.geodesic"></script>
 
-		const map = L.map('map').setView([20, 0], 3);
 
-		const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 19,
-			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-		}).addTo(map);
-			  
-		<?php
-			 foreach ($flightlist as $key => $value) {
-				echo 'const flight_'.$key.' = new L.Geodesic([['.$value['from']['lat'].', '.$value['from']['lon'].'], ['.$value['to']['lat'].', '.$value['to']['lon'].']]).addTo(map);';
-			}
-		?>
-	</script>
-  </body>
+    <style>
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+        }
+
+        .leaflet-container {
+            height: 100%;
+            width: 100%;
+            max-width: 100%;
+            max-height: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="map"></div>
+    <script>
+        const map = L.map('map').setView([20, 0], 3);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        <?php
+        foreach ($flightlist as $key => $value) {
+            echo 'const flight_' . $key . ' = new L.Geodesic([[' . $value['from']['lat'] . ', ' . $value['from']['lon'] . '], [' . $value['to']['lat'] . ', ' . $value['to']['lon'] . ']]).addTo(map);';
+        }
+        ?>
+    </script>
+</body>
+
 </html>
